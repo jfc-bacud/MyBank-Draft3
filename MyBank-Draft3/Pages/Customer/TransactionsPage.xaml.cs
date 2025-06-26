f0ba74fd55f33dedc0155d307ca553c56a6c55fa
jfc-bacud <bacud.jfc@gmail.com>
2025-06-27 05:49:15 +0800

    sasasasa

--- MyBank-Draft3/Pages/Customer/TransactionsPage.xaml.cs
+++ MyBank-Draft3/Pages/Customer/TransactionsPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using MyBank_Draft3.Classes;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,38 +25,63 @@ namespace MyBank_Draft3.Pages.Customer
     {
         string localUID;
         Database _localdb;
+        public ObservableCollection<TransactionListView> _recordedTransactions { get; set; }
 
         public TransactionsPage(string UID)
         {
+            _recordedTransactions = new ObservableCollection<TransactionListView>();
             localUID = UID;
             InitializeComponent();
             LoadDatabase();
         }
 
         private void LoadDatabase()
-        {
+        {          
             _localdb = new Database();
+            this.DataContext = this;
             FetchTransactions();
-
+          
         }
 
         private void FetchTransactions()
         {
-            var _transactions = from t in _localdb.db.Transactions
+            var _transactions = (from t in _localdb.db.Transactions
                                 where t.User_ID == localUID
                                 join c in _localdb.db.Categories on t.Category_ID equals c.Category_ID
-                                select new 
+                                select new TransactionListView
                                 {
-                                    c.Category_Desc,
-                                    t.Transaction_Date,
-                                    t.Amount,
-                                    c.Category_Type,
-                                    t.Transaction_Desc,
-                                };
+                                    Transact = c.Category_Desc,
+                                    Date = t.Transaction_Date,
+                                    Amount = t.Amount,
+                                    Type = c.Category_Type,
+                                    //TransactionDesc = t.Transaction_Desc
+                                }).ToList();
 
-            ListViewData.ItemsSource = _transactions.ToList();
+            _recordedTransactions.Clear();
 
+            foreach (var t in _transactions)
+            {
+                _recordedTransactions.Add(t);
+            }
 
         }
+
+        //private void CreateItemSource(IQueryable<TransactionListView> transactions)
+        //{
+        //    DataTable itemSource = new DataTable();
+
+        //    itemSource.Columns.Add("Transaction", typeof(string));
+        //    itemSource.Columns.Add("Date", typeof(DateTime));
+        //    itemSource.Columns.Add("Amount", typeof(decimal));
+        //    itemSource.Columns.Add("Type", typeof(string));
+
+        //    foreach (var t in transactions)
+        //    {
+        //        itemSource.Rows.Add(t.CategoryDesc, t.TransactionDate, t.Amount, t.CategoryType);
+        //    }
+
+        //    ListViewData.ItemsSource = itemSource.DefaultView;
+
+        //}
     }
 }
